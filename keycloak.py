f2d9ea816760ef97e7f45af5cbdfbafc116f1e2c
Amit Kumar <dtu.amit@gmail.com>
2023-06-30 13:17:20 +0100

    cleanup and keycloak client

--- keycloak.py
+++ keycloak.py
@@ -14,42 +14,43 @@ KEYCLOAK_URL = f"{BASE_URL}/auth/admin/realms/{REALM_NAME}/users"
 AUTH_URL = f"{BASE_URL}/auth/realms/master/protocol/openid-connect/token"
 
 
-def get_access_token():
-    auth_data = {
-        "grant_type": "client_credentials",
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET
-    }
-    response = requests.post(AUTH_URL, data=auth_data)
-    response_data = response.json()
-    access_token = response_data["access_token"]
-    return access_token
-
-
-def create_user(access_token, user_data):
-    # Create headers for the API request
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {access_token}"
-    }
-
-    # Make the API request to create the user
-    response = requests.post(
-        KEYCLOAK_URL,
-        headers=headers, data=json.dumps(user_data)
-    )
-    # Check the response status
-    if response.status_code == 201:
-        logging.info("User created successfully!")
-    else:
-        logging.info(f"Failed to create user. Status code: {response.status_code}")
-        logging.info(f"Response: {response.text}")
-    return response
-
-
 def generate_deterministic_uuid(text, salt="nebari-gh-random"):
     text_ = f"{text}-{salt}"
     return uuid.uuid5(uuid.NAMESPACE_URL, text_).hex
+
+
+class KeyCloakClient:
+    def __init__(self, realm_url, auth_url):
+        self.realm_url = realm_url
+        self.auth_url = auth_url
+
+    def get_access_token(self):
+        auth_data = {
+            "grant_type": "client_credentials",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET
+        }
+        response = requests.post(self.auth_url, data=auth_data)
+        response_data = response.json()
+        access_token = response_data["access_token"]
+        return access_token
+
+    def create_user(self, user_data):
+        access_token = self.get_access_token()
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}"
+        }
+        response = requests.post(
+            self.realm_url,
+            headers=headers, data=json.dumps(user_data)
+        )
+        if response.status_code == 201:
+            logging.info("User created successfully!")
+        else:
+            logging.info(f"Failed to create user. Status code: {response.status_code}")
+            logging.info(f"Response: {response.text}")
+        return response
 
 
 def setup_logging():
@@ -65,7 +66,7 @@ def setup_logging():
 
 def main():
     setup_logging()
-    access_token = get_access_token()
+    kclient = KeyCloakClient(realm_url=KEYCLOAK_URL, auth_url=AUTH_URL)
     users = json.load(open('users.json', 'r'))
     total_users = len(users)
     for idx, user in enumerate(users):
@@ -85,7 +86,7 @@ def main():
                     "temporary": False
                 }]
             }
-            create_user(access_token, user_data)
+            kclient.create_user(user_data)
         except Exception as e:
             logging.info(f"Failed to create user: {user}")
             logging.info(e)
