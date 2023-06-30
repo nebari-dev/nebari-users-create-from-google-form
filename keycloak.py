d60a6fcaf6756bc27ce92903806553eeda1459da
Amit Kumar <dtu.amit@gmail.com>
2023-06-30 14:42:13 +0100

    fix keycloak urls

--- keycloak.py
+++ keycloak.py
@@ -10,8 +10,9 @@ BASE_URL = os.environ["BASE_URL"]
 CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
 
-KEYCLOAK_USERS_URL = f"{BASE_URL}/auth/admin/realms/{REALM_NAME}/users"
-KEYCLOAK_GROUPS_URL = f"{BASE_URL}/auth/admin/realms/{REALM_NAME}/groups"
+KEYCLOAK_REALM_URL = f"{BASE_URL}/auth/admin/realms/{REALM_NAME}"
+KEYCLOAK_USERS_URL = f"{KEYCLOAK_REALM_URL}/users"
+KEYCLOAK_GROUPS_URL = f"{KEYCLOAK_REALM_URL}/groups"
 
 KEYCLOAK_AUTH_URL = f"{BASE_URL}/auth/realms/master/protocol/openid-connect/token"
 
