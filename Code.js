b25e8e9ac63f49c9cc6305fad9b3c9fc37b58cbe
Amit Kumar <dtu.amit@gmail.com>
2023-07-04 15:09:09 +0100

    trigger lambda directly

--- Code.js
+++ Code.js
@@ -8,85 +8,54 @@ function decodeBase64(base64String) {
     return Utilities.newBlob(decodedBytes).getDataAsString();
 }
 
+
+function sendApiRequest(username, password, coupon, pyvista) {
+    var url = "https://4y6bjkv5z0.execute-api.us-east-2.amazonaws.com/";
+    var headers = {
+        "Content-Type": "application/json"
+    };
+
+    var payload = {
+        "username": username,
+        "password": password,
+        "pyvista": pyvista,
+        "coupon": coupon
+    };
+
+    var options = {
+        "method": "post",
+        "headers": headers,
+        "payload": JSON.stringify(payload)
+    };
+
+    Logger.log("payload");
+    Logger.log(payload);
+    var response = UrlFetchApp.fetch(url, options);
+
+    // Handle the response
+    var responseData = JSON.parse(response.getContentText());
+    Logger.log("responseData");
+    Logger.log(responseData);
+}
+
+
 function sendFormDataToGitHub(formAttributes) {
-    const scriptProperties = PropertiesService.getScriptProperties();
-    const githubToken = scriptProperties.getProperty('GITHUB_TOKEN');
-
-    var owner = scriptProperties.getProperty('GH_REPO_OWNER');
-    var repo = scriptProperties.getProperty('GH_REPO');
-    var filePath = 'users.json';
-
     var formData = {}
     for (var i = 0; i < formAttributes.length; i++) {
         var attribute = formAttributes[i];
         var name = attribute.getItem().getTitle();
         formData[name] = attribute.getResponse();
     }
-
     Logger.log("formAttributes");
     Logger.log(formData);
-    var apiUrl = 'https://api.github.com/repos/' + owner + '/' + repo + '/contents/' + filePath;
-    var response = UrlFetchApp.fetch(apiUrl, {
-        headers: {
-            Authorization: 'Bearer ' + githubToken
-        },
-        muteHttpExceptions: true
-    });
-    var fileInfo = JSON.parse(response.getContentText());
-    Logger.log("fileInfo");
-    Logger.log(fileInfo);
-    var sha = fileInfo.sha;
+    var Username = formData['Username']
+    var Password = formData['Password']
+    var Coupon = formData['Coupon']
+    var PyvistaVal = formData['Attending PyVista Tutorial?']
 
-    Logger.log(fileInfo.content);
-    Logger.log("fileInfo.content");
-
-    var currentContent = decodeBase64(fileInfo.content);
-    Logger.log("currentContent");
-    Logger.log(currentContent);
-    Logger.log("formData");
-    Logger.log(formData);
-
-    var updatedContent;
-    var existingData;
-    try {
-        if (currentContent) {
-            // If file exists and contains valid JSON data
-            existingData = JSON.parse(currentContent);
-            Logger.log("existingData");
-            Logger.log(existingData);
-            if (!Array.isArray(existingData)) {
-                throw new Error('Invalid existing JSON data');
-            }
-            existingData.push(formData);
-        } else {
-            // If file doesn't exist, create new JSON array with form data
-            existingData = [formData];
-        }
-        updatedContent = JSON.stringify(existingData, null, 2);
-    } catch (error) {
-        // Handle JSON parsing errors
-        Logger.log('Error parsing existing JSON content: ' + error);
-        return;
+    var Pyvista = false
+    if (PyvistaVal.toLowerCase() === "yes") {
+        Pyvista = true;
     }
-
-    var encodedContent = Utilities.base64EncodeWebSafe(updatedContent);
-
-    // Prepare request options
-    var commitMessage = 'Update users.json from google form data'
-    var options = {
-        method: 'PUT',
-        headers: {
-            Authorization: 'Bearer ' + githubToken,
-            'Content-Type': 'application/json'
-        },
-        payload: JSON.stringify({content: encodedContent, sha: sha, message: commitMessage})
-    };
-
-    // Send request to GitHub API
-    var apiUrl = 'https://api.github.com/repos/' + owner + '/' + repo + '/contents/' + filePath;
-    Logger.log(apiUrl);
-    var response = UrlFetchApp.fetch(apiUrl, options);
-
-    // Log response
-    Logger.log(response.getContentText());
+    sendApiRequest(Username, Password, Coupon, Pyvista)
 }
